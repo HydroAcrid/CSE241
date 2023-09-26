75fbc78b6c969db003b36b94b499b99b4282f644
HydroAcrid <kevindotel21@gmail.com>
2023-09-26 12:41:17 -0400

    trying to connect to database

--- HW4/ked225Dotel/capacity.java
+++ HW4/ked225Dotel/capacity.java
@@ -1,8 +1,42 @@
 import java.util.*;
+import java.sql.*;
+
 
 public class capacity {
+    //Database URL
+    static final String DB_URL = "jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241";
+
     public static void main(String[] args) {
-       System.out.println();
+        //Variable setup
+        String user;
+        String password;
+        Connection connection = null;
+        Scanner scnr = new Scanner(System.in);
+       
+        //Intro Message
+        System.out.println("Welcome to my HW 4 Program!!! -Kevin Dotel Inc.");
+
+        do {
+            try {
+                //Login info.
+                System.out.print("Please Enter Username: ");
+                user = scnr.nextLine();
+                System.out.print ("Please Enter Password: ");
+                password = scnr.nextLine();
+
+                //Trying to connect to database
+                connection = DriverManager.getConnection(DB_URL, user, password);
+                System.out.println("Connection Successful. Epic.");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } while (connection == null);
+        
+
+
+
+        
     }
 }
 
