7f7c059f0f66579a4f97e73a1f3a3cea3fcb9ab0
HydroAcrid <kevindotel21@gmail.com>
2023-09-28 21:59:15 -0400

    added the section testing chunk

--- HW4/ked225Dotel/capacity.java
+++ HW4/ked225Dotel/capacity.java
@@ -12,16 +12,20 @@ public class capacity {
         String password;
         Connection connection = null;
         Scanner scnr = new Scanner(System.in);
+
+        int section; //Must be a year a.k.a 4 digits 
+
        
         //Intro Message
         System.out.println("Welcome to my HW 4 Program!!! -Kevin Dotel Inc.");
 
+        //Try to connect to oracle database 
         do {
             try {
                 //Login info.
-                System.out.print("Please Enter Username: ");
+                System.out.print("Enter Oracle user id: ");
                 user = scnr.nextLine();
-                System.out.print ("Please Enter Password: ");
+                System.out.print ("Enter Oracle password for dbcourse: ");
                 password = scnr.nextLine();
 
                 //Trying to connect to database
@@ -32,6 +36,29 @@ public class capacity {
                 e.printStackTrace();
             }
         } while (connection == null);
+
+
+        //Asking for section -> classroom capacity 
+        //Make this a loop
+        boolean valid = false; 
+        do {
+            try {
+                //Ask for section
+                System.out.println("Input data on the section whose classroom capacity you wish to check.");
+                System.out.println("Year(yyyy) or 0 to exit: ");
+                section = scnr.nextInt();
+
+                //If they press 0
+                if(section == 0) {
+                    System.out.println("Exiting...");
+                    break; 
+                }
+
+            } catch(Exception e) {
+                System.out.println("Invalid input. Please enter a valid year or 0 to exit.");
+                scnr.nextLine(); // Clear the scanner buffer
+            }
+        } while(!valid);
         
 
 
