be0c2f43b376ff23db918389b1fcf941bd8d4b68
HydroAcrid <kevindotel21@gmail.com>
2023-11-16 17:01:38 -0500

    added my lib file

--- Final/ked225Dotel/main.java
+++ Final/ked225Dotel/main.java
@@ -3,6 +3,9 @@ import java.util.*;
 import java.sql.*;
 
 public class main {
+    //Database URL
+    static final String DB_URL = "jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241";
+
     public static void main(String[] args) {
         
     }
