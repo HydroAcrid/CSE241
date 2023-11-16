aec02b3059c1fa03636920d6563b10384137a15e
HydroAcrid <kevindotel21@gmail.com>
2023-11-16 17:07:58 -0500

    added connection to database

--- Final/ked225Dotel/main.java
+++ Final/ked225Dotel/main.java
@@ -7,6 +7,33 @@ public class main {
     static final String DB_URL = "jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241";
 
     public static void main(String[] args) {
+        //Variable setup
+        String user;
+        String password;
+        Connection connection = null;
+        Scanner scnr = new Scanner(System.in);
+
+        //Make all of the table variables here 
+
+        //Try to connect to oracle database 
+        do {
+            try {
+                //Login info.
+                System.out.print("Enter Oracle user id: ");
+                user = scnr.nextLine();
+                System.out.print ("Enter Oracle password for dbcourse: ");
+                password = scnr.nextLine();
+
+                //Trying to connect to database
+                connection = DriverManager.getConnection(DB_URL, user, password);
+                System.out.println("Connection Successful. Epic.");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } while (connection == null);
+
+
         
     }
 }
