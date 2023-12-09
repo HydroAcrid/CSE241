56425e4319162a2f6391cee54bdb694945b023fd
HydroAcrid <kevindotel21@gmail.com>
2023-12-08 20:08:28 -0500

    added a new propmanager function

--- Final/ked225Dotel/interfaces/PropertyManager.java
+++ Final/ked225Dotel/interfaces/PropertyManager.java
@@ -41,7 +41,7 @@ public class PropertyManager {
                     recordVisitData(scnr);
                     break;
                 case 4:
-                    createNewLease(scnr);
+                    recrodLeaseData(scnr);
                     break;
                 case 5:
                     recordMoveOutDate();
@@ -61,7 +61,7 @@ public class PropertyManager {
     
 
     //Method to create a lease. First you need to make a tenant though because it is a full participation relationship 
-    public static void createNewLease(Scanner scnr) {
+    public static void recrodLeaseData(Scanner scnr) {
         int tenantId = addPersonToLease(scnr);
 
         LocalDate startDate = null;
@@ -211,123 +211,6 @@ public class PropertyManager {
         }
     }
     
-    
-
-    public static void recordLeaseData(Scanner scnr) {
-        System.out.println("Record Lease Data");
-    
-        int tenantId;
-        String leaseStartDate;
-        String leaseEndDate;
-        double rentAmount;
-    
-        // Get Tenant ID
-        do {
-            System.out.print("Enter Tenant ID: ");
-            while (!scnr.hasNextInt()) {
-                System.out.println("Invalid input. Please enter a number.");
-                scnr.next(); // discard invalid input
-            }
-            tenantId = scnr.nextInt();
-            scnr.nextLine(); // Consume the newline left-over
-            if (!isTenantExists(tenantId)) {
-                System.out.println("Tenant ID does not exist. Please try again.");
-            }
-        } while (!isTenantExists(tenantId));
-    
-        // Get Lease Start Date
-        System.out.print("Enter Lease Start Date (YYYY-MM-DD): ");
-        leaseStartDate = scnr.nextLine();
-    
-        // Get Lease End Date
-        System.out.print("Enter Lease End Date (YYYY-MM-DD): ");
-        leaseEndDate = scnr.nextLine();
-    
-        // Get Rent Amount
-        do {
-            System.out.print("Enter Rent Amount: ");
-            while (!scnr.hasNextDouble()) {
-                System.out.println("Invalid input. Please enter a number.");
-                scnr.next(); // discard invalid input
-            }
-            rentAmount = scnr.nextDouble();
-            scnr.nextLine(); // Consume the newline left-over
-            if (rentAmount <= 0) {
-                System.out.println("Rent amount must be greater than 0. Please try again.");
-            }
-        } while (rentAmount <= 0);
-    
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-    
-        try {
-            // Get the database connection
-            conn = DatabaseUtil.getConnection();
-    
-            // SQL to insert new lease
-            String sql = "INSERT INTO Lease (lease_start_date, lease_end_date, rent_amt, ten_id) " +
-                         "VALUES (TO_DATE(?, 'YYYY-MM-DD'), TO_DATE(?, 'YYYY-MM-DD'), ?, ?)";
-    
-            // Create PreparedStatement
-            pstmt = conn.prepareStatement(sql);
-    
-            // Set parameters
-            pstmt.setString(1, leaseStartDate);
-            pstmt.setString(2, leaseEndDate);
-            pstmt.setDouble(3, rentAmount);
-            pstmt.setInt(4, tenantId);
-    
-            // Execute update
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Lease data recorded successfully.");
-            } else {
-                System.out.println("Failed to record lease data.");
-            }
-        } catch (SQLException e) {
-            System.out.println("Database error occurred.");
-            e.printStackTrace();
-        } finally {
-            // Close resources
-            try {
-                if (pstmt != null) pstmt.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-    
-    // Helper method to check if a tenant exists
-    private static boolean isTenantExists(int tenantId) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        boolean exists = false;
-    
-        try {
-            conn = DatabaseUtil.getConnection();
-            String sql = "SELECT COUNT(*) FROM Tenant WHERE ten_id = ?";
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, tenantId);
-            rs = pstmt.executeQuery();
-            if (rs.next() && rs.getInt(1) > 0) {
-                exists = true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (pstmt != null) pstmt.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-    
-        return exists;
-    }
-    
-
     public static int addPersonToLease(Scanner scnr) {
         System.out.println("Adding a new tenant...");
     
@@ -374,6 +257,7 @@ public class PropertyManager {
                 System.out.println("Tenant successfully added.");
     
                 // Retrieve the tenantId of the newly added tenant
+                // REMEMBER TO FIX THIS MEMORY LEAK
                 String selectSql = "SELECT ten_id FROM Tenant WHERE email_addr = ?";
                 pstmt = conn.prepareStatement(selectSql);
                 pstmt.setString(1, emailAddr);
@@ -400,8 +284,8 @@ public class PropertyManager {
         return tenantId; // Return the retrieved tenantId
     }
     
-    
 
+    // Method to add a pet to a tenant, which in turn adds it to the lease
     private static void addPetToLease(Scanner scanner) {
         System.out.println("Add a Pet to a Lease");
 
@@ -466,6 +350,7 @@ public class PropertyManager {
         }
     }
 
+    // Method to make sure tenant exists when adding a pet 
     private static boolean checkTenantIdExists(int tenantId) {
         Connection conn = null;
         PreparedStatement pstmt = null;
